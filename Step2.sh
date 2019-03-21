878c64281407729caa33e97fc501e7b9ab748185
everbiome <everbiome@gmail.com>
2019-03-21 11:05:03 -0400

    Update Step2.sh

--- Step2.sh
+++ Step2.sh
@@ -1,32 +1,52 @@
 #!bin/bash
-
-#!bin/bash
+#Required tools:BBmap, umi_tools, QIIME2
 #Folders: filtered merged split qiime2
 #Extract barcodes for pair-end read1 and read2
 echo 'Merge filtered R1 and R2 pair-end reads'
 ./bbmap/bbmerge.sh in1=filtered/R1.fq.gz in2=filtered/R2.fq.gz out=merged/merged.fq.gz
 echo 'Extract barcodes from merged pair-end reads'
+#Step1 We will work on the reads with mixed RF barcodes in each pair-end fastqfile
+#extract 9 base (8 base barcodes + 1 RF identifier from primer) from 5'end
 umi_tools extract --stdin=merged/merged.fq.gz --bc-pattern=NNNNNNNNN  --stdout=split/split1.fq.gz
+#trim primer from 5''end
 ./bbmap/bbduk.sh in=split/split1.fq.gz out=split/split12.fq.gz ftl=18
+#remove header info after first whitespace
 ./bbmap/reformat.sh n=split/split12.fq.gz out=split/split123.fq.gz trd
+#reverse the reads
 ./bbmap/reformat.sh in=split/split123.fq.gz out=split/split1234.fq.gz rcomp
+#extract 9 base (8 base barcodes + 1 RF identifier from primer) from 5'end of reversed reads
 umi_tools extract --stdin=split/split1234.fq.gz --bc-pattern=NNNNNNNNN --stdout split/split12345.fq.gz
+#trim primer from 5''end of reversed reads
 ./bbmap/bbduk.sh in=split/split12345.fq.gz out=split/R1R2.fq.gz ftl=18
+#delete temp files
 rm split/s*.*
+#Step 2 We will reverse the original fastq file and extract the barcode again
+#revised the meregd reads
 ./bbmap/reformat.sh in=merged/merged.fq.gz out=merged/mergedr.fq.gz rcomp
+#extract 9 base (8 base barcodes + 1 RF identifier from primer) from 5'end
 umi_tools extract --stdin=merged/mergedr.fq.gz --bc-pattern=NNNNNNNNN  --stdout=split/split1.fq.gz
+#trim primer from 5''end
 ./bbmap/bbduk.sh in=split/split1.fq.gz out=split/split12.fq.gz ftl=18
+#remove header info after first whitespace
 ./bbmap/reformat.sh n=split/split12.fq.gz out=split/split123.fq.gz trd
+#reverse the reads
 ./bbmap/reformat.sh in=split/split123.fq.gz out=split/split1234.fq.gz rcomp
+#extract 9 base (8 base barcodes + 1 RF identifier from primer) from 5'end of reversed reads
 umi_tools extract --stdin=split/split1234.fq.gz --bc-pattern=NNNNNNNNN --stdout split/split12345.fq.gz
+#trim primer from 5''end of reversed reads
 ./bbmap/bbduk.sh in=split/split12345.fq.gz out=split/R2R1.fq.gz ftl=18
+#delete temp files
 rm split/s*.*
+#combine the results from two steps
 cat split/R2R1.fq.gz split/R1R2.fq.gz > split.fq.gz
+$remove temp files
 rm split/R*.*
-#Demultiplex reads
+
+#Demultiplex reads, require barcode.txt
 ./bbmap/demuxbyname.sh in=split.fq.gz out=split/%.fq prefixmode=f names=barcode.txt
-rm split.fq.gz
-#load qiime2
+#rm split.fq.gz
+
+#load qiime2, require fq33.csv and metadata.tsv as directed by qiime2 protocol
 echo 'Start QIIME2'
 Conda activate qiime2
 echo 'Import demux filtered merged reads'
